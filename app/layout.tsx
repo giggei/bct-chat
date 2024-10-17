cb503d36783605e0e225583786b3935ea7653c8e
Markus Garnitz <g@rnitz.de>
2024-10-17 08:49:37 +0200

    Changed App Name

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { assistantId } from "./assistant-config";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Assistants API Quickstart",
-  description: "A quickstart template using the Assistants API with OpenAI",
+  title: "BayernCloud Tourismus Experte",
+  description: "Stelle Fragen zur BayernCloud Tourismus",
   icons: {
     icon: "/bayern.svg",
   },
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         {assistantId ? children : <Warnings />}
-        <img className="logo" src="/bayern.svg" alt="OpenAI Logo" />
+        <img className="logo" src="/bayern.svg" alt="Bayern Logo" />
       </body>
     </html>
   );
